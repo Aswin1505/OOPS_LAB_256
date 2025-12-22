266487191aed98c88f1218e4566c7b7b14886b85
Aswin1505 <ctraswin@gmail.com>
2025-12-22 12:36:27 +0530

    Added retail_shop.java
    Added 2nd java program that gives a result of final price of all 3 input items

---
+++ retail_shop.java
@@ -0,0 +1,9 @@
+class retail_shop
+{
+public static void main(String args[])
+{int item_a=100, item_b=200, item_c=500;
+double price;
+price=(item_a*2)-(item_b/10)+(item_c);
+System.out.println(price);
+}
+}
